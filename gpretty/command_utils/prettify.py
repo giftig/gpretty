03a8ec55301586801209d0131d487a7c97b5ff74
Rob Moore <rob.moore@adstream.com>
2017-09-21 17:21:16 +0100

    Add cyan

--- gpretty/command_utils/prettify.py
+++ gpretty/command_utils/prettify.py
@@ -8,6 +8,7 @@ class ColourMixin(object):
         'red': '\033[31m',
         'green': '\033[32m',
         'yellow': '\033[33m',
+        'cyan': '\033[36m',
         'white': '\033[37m',
         'grey': '\033[90m',
         'reset': '\033[0m'
